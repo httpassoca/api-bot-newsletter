e220a17499fac88c416eb5feb88be593af839a9d
Rafael <trabsom.rafael@gmail.com>
2021-02-14 08:16:35 -0300

    add arrayOfTweets function

---
+++ src/helpers.ts
@@ -0,0 +1,25 @@
+const arrayOfTweets = (string: string) => {
+  const array: string[] = [];
+  let part;
+  let pos;
+  let betterPart;
+  let diff;
+
+  while (string.length > 280) {
+    part = string.substr(0,280);
+    pos = part.lastIndexOf(" ");
+    diff = 280 - pos;
+    if(diff <= 2) {
+      part = string.substr(0,277);
+      pos = part.lastIndexOf(" ");
+    }
+    betterPart = string.substr(0, pos) + ' +';
+    string = string.slice(pos + 1, string.length);
+    console.log(betterPart);
+    array.push(betterPart);
+  }
+  array.push(string);
+  return array;
+}
+
+export default arrayOfTweets;
