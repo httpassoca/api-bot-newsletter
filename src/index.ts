b129071b141bef1c54e989b4de1d93f3940c4b7b
Rafael <trabsom.rafael@gmail.com>
2021-02-13 23:23:54 -0300

    add tweet function

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,15 @@ require('dotenv/config');
   const browser = await launch({headless: false});
   const page = await browser.newPage();
   await page.goto('https://twitter.com/login');
-  await page.waitForTimeout(2000);
+  await page.waitForTimeout(1000);
   await page.type('input', 'DeschampsNews', { delay: 110 });
   await page.type('input[type="password"]', process.env.PASSWORD as string,  { delay: 110 });
   await page.keyboard.press('Enter');
+  await page.waitForTimeout(4000);
+  await page.screenshot({ path: "src/screenshot.png" })
+  await page.click('[data-contents] > div > div');
+  await page.keyboard.type('DeschampsNews', { delay: 110 });
+  await page.click('div[role="button"][data-testid="tweetButtonInline"]');
+  
   // await browser.close()
 })();
