2b6bfa83bc38b071ec989968474440534b49a40e
Rafael <trabsom.rafael@gmail.com>
2021-04-01 00:54:42 -0300

    add email verification

--- src/services.ts
+++ src/services.ts
@@ -7,8 +7,17 @@ export const Login = async (page: Page) => {
   await page.type('input', 'DeschampsNews', { delay: 65 });
   await page.type('input[type="password"]', process.env.PASSWORD as string,  { delay: 65 });
   await page.keyboard.press('Enter');
-  console.log('LOGGED BITCHESSSS 😎');
-  await page.waitForTimeout(4000);
+  console.log('Logging in... 😎');
+  await page.waitForTimeout(3000);
+  try {
+    await page.$eval('[href="https://support.twitter.com/articles/20171250-im-being-asked-for-additional-information-when-i-sign-up-or-log-in?ref=password_reset"]', e => e.outerHTML);
+    await page.keyboard.type(process.env.EMAIL as string, { delay: 15 });
+    await page.keyboard.press('Enter');
+    console.log('Verificando email...');
+    await page.waitForTimeout(3500);
+} catch (error) {
+    console.log('Sem verificação de e-mail necessária 😋');
+  }
 }
 
 export const Tweet = async (page: Page, tweet: string[]) => {
@@ -21,7 +30,8 @@ export const Tweet = async (page: Page, tweet: string[]) => {
       await page.keyboard.type(tweets[i], { delay: 15 });
       await page.click(primeiro ? 'div[role="button"][data-testid="tweetButtonInline"]' : 'div[role="button"][data-testid="tweetButton"]');
       await page.waitForTimeout(2000);
-      console.log('a tweet 🤗');
+      console.log('Tweet! 🤗');
+
     }
   }
 }
