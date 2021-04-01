8b19c7fe5b347c4179ebcabbe3e3369757ac2795
Rafael <trabsom.rafael@gmail.com>
2021-04-01 00:22:58 -0300

    remove headless

--- src/routes.ts
+++ src/routes.ts
@@ -6,7 +6,7 @@ export default (app: Express) => {
   app.post("/newsletter", async (req: Request, res: Response) => {
     console.log('Route newsletter 😎');
     const { news } = req.body;
-    const browser = await launch({headless: false});
+    const browser = await launch({headless: true});
     const page = await browser.newPage();
     await Login(page);
     await Tweet(page, news);
